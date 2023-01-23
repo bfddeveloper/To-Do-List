7b0cbfadb0d3bcce31534ae157685bb756995aeb
bfddeveloper <dicksonbrody@icloud.com>
2023-01-23 09:52:01 -0600

    set up list edit functionality

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["first thing", "second thing", "third thing", "fourth thing", "fifth thing"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(things, id: \.self) { thing in
+                    Text(thing)
+                }
+                .onMove{ indices, newOffset in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete{ IndexSet in
+                    things.remove(atOffsets: IndexSet)
+                }
+            }
+            .navigationBarTitle("Things", displayMode: .inline)
         }
         .padding()
     }
